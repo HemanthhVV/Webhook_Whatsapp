2646516517e2a214c902041802511d4a90b34f8e
hemanthhvv <vvhemanthh@gmail.com>
2025-01-17 11:51:22 +0530

    json body modified

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post("/webhook", (req, res) => { //i want some
         ) {
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages
+            let msg_body = JSON.stringify(body_param.entry[0].changes[0].value.messages[0],null,2)
 
             sendToClients(msg_body)
 
